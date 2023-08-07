5aa79590969cacd3b6baf7ae46a205a1f0c25825
tusharpareek55 <129362920+tusharpareek55@users.noreply.github.com>
2023-08-07 11:37:13 +0530

    Update index.js

--- My-awesome-nerdpack/nerdlets/home/index.js
+++ My-awesome-nerdpack/nerdlets/home/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import { Counter } from './components/counter';
-
+import Linechart from './components/linechart';
+import Piechart from './components/piechart';
+import Areachart from './components/areachart';
 // https://docs.newrelic.com/docs/new-relic-programmable-platform-introduction
 
 export default class HomeNerdlet extends React.Component {
   render() {
     return(
       <div>
-        <Counter />
+         <h2>Line Chart</h2>
+        <Linechart />
+        <h2>Pie chart</h2>
+        <Piechart />
+        <h2>Area Chart</h2>
+        <Areachart />
       </div>
     );
   }
 }
-
 
 
